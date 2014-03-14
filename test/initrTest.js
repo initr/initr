a6990003f6799ec397bb2d2bc51c079ac6b9f85c
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-14 01:45:13 -0500

    Why is this failing?

--- test/initrTest.js
+++ test/initrTest.js
@@ -1,20 +1,40 @@
 var expect = require('chai').expect;
-var mockery = require('mockery');
+var sinon = require('sinon');
+var mock = sinon.mock;
 
-suite('Initr', function() {
+describe('Initr', function() {
+	var Initr, fs, cli, childProcess, initr, mockJSON;
+
 	setup(function() {
-		mockery.enable();
+		Initr = require('../lib/initr.js');
+		fs = require('fs');
+		cli = require('cli');
+		childProcess = require('child_process');
+		initr = Initr(fs, cli, childProcess);
 
-		var Initr = require('../lib/initr.js');
-		var fs = require('fs');
-		var cli = require('cli');
-		var childProcess = require('child_process');
-		var initr = Initr(fs, cli, childProcess);
+		mockJSON = {
+			"commands": [
+				"ln -s bower_components/jquery/js/jquery.js public/js/jquery.js"
+			],
+			"scripts": [
+				"init/startup.sh",
+				"https://github.com/rtablada/initr/tree/master/tests/test.sh"
+			],
+			"require": {
+				"rtablada/gumby": "*"
+			}
+		};
 	});
 
 	describe('getConfigOptions()', function() {
-		it('should return json parsed config file', function() {
+		it('should return standard json parsed config file', function() {
+			fsMock = mock(fs);
+			fsMock.expects('existsSync').once().returns(true);
+			fsMock.expects('readFileSync').once().returns(JSON.stringify(mockJSON));
 
+			options = initr.getConfigOptions();
+			fsMock.verify();
 		});
 	});
+
 });
